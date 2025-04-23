61d61e0b7071ed41682cb10b6547a04dc04c08cb
Bas Milius <basmilius@hotmail.com>
2025-04-23 11:04:03 +0200

    chore: abstract away oxc dts generation. chore: bump deps. chore: bump version.

--- build.ts
+++ build.ts
@@ -1,47 +1,5 @@
-import { build, type BunPlugin, file, pathToFileURL, write } from 'bun';
-import { isolatedDeclaration } from 'oxc-transform';
-
-function generateTypesPlugin(): BunPlugin {
-    const visited = new Set<string>();
-
-    return {
-        name: 'oxc-transform-dts',
-
-        setup(builder) {
-            if (!builder.config.root || !builder.config.outdir) {
-                console.warn('osx-transform-dts requires both a root and outdir.');
-                return;
-            }
-
-            const rootPath = pathToFileURL(builder.config.root).pathname;
-            const outPath = pathToFileURL(builder.config.outdir).pathname;
-
-            builder.onStart(() => {
-                visited.clear();
-            });
-
-            builder.onLoad({filter: /\.ts$/}, async args => {
-                if (args.path.startsWith(rootPath) && !visited.has(args.path)) {
-                    visited.add(args.path);
-
-                    const {code} = isolatedDeclaration(
-                        args.path,
-                        await file(args.path).text()
-                    );
-
-                    await write(
-                        args.path
-                            .replace(new RegExp(`^${rootPath}`), outPath)
-                            .replace(/\.ts$/, '.d.ts'),
-                        code
-                    );
-                }
-
-                return undefined;
-            });
-        }
-    };
-}
+import { generateDts } from '@basmilius/vite-vue-preset/oxc';
+import { build } from 'bun';
 
 try {
     await build({
@@ -59,13 +17,13 @@ try {
             'pinia'
         ],
         plugins: [
-            generateTypesPlugin()
+            generateDts()
         ]
     });
 } catch (err) {
-    console.error('Build failed!');
+    console.error('✘ Build failed!');
     console.error(err);
     process.exit(1);
 }
 
-console.log('Build complete!');
+console.log('✔ Build complete!');
